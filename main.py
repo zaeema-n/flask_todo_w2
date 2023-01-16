19bb1f3771231da347743fa0b7e78510790ffc6d
Zaeema Nashath <znas0003@student.monash.edu>
2023-01-16 12:12:16 +0530

    Fixed CORS policy issue

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
